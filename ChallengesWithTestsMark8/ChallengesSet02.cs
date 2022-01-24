6bc22741f95d776595cf5ce9e64f9dda0da90f71
brittany melton <brittanymelton26@gmail.com>
2022-01-24 18:30:56 -0500

    week 2 challenges complete

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,126 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+           var alphabet = "abcdefghijklmnopqrstuvwxyz";
+           string d = c.ToString().ToLower();
+
+            foreach (char letter in alphabet)
+            {
+                if (letter.ToString() == d)
+                    
+                   return true;             
+            }
+            return false;
+
+            //throw new NotImplementedException();
         }
 
-        public bool CountOfElementsIsEven(string[] vals)
+        public bool CountOfElementsIsEven(string[] vals)           
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0; //? true : false;
+            /* if (vals.Length % 2 == 0)
+             {
+             return true;
+             }    
+
+             return false;*/
+
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+                return true;
+            return false;
+            
+            //throw new NotImplementedException();
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+                return true;
+            return false;
+
+            //throw new NotImplementedException();
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+                return 0;
+            else
+            return numbers.Min() + numbers.Max();
+            
+            //throw new NotImplementedException();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+
+            return str1.Length < str2.Length ? str1.Length : str2.Length;
+            //throw new NotImplementedException();
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            return (numbers == null) ? 0 : numbers.Sum();
+            //throw new NotImplementedException();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+           int sum = 0; 
+            if(numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            
+                if(numbers[i] % 2 == 0)
+                
+                    sum += numbers[i];
+                    
+            return sum;
+
+
+            //throw new NotImplementedException();
         }
 
         public bool IsSumOdd(List<int> numbers)
-        {
-            throw new NotImplementedException();
+        {   int sum = 0;    
+            if(numbers == null)
+            
+                return false;
+            
+            for (int i = 0; i < numbers.Count; i++)
+               
+                sum += numbers[i];
+                    if (sum % 2 != 0)
+                        return true;
+                    if(sum % 2 == 0)
+                        return false;
+                
+           return false;
+                
+
+            //throw new NotImplementedException();
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long result = number / 2;
+            if (result <= 0)
+                return 0;
+            else
+            return result;
+           
+        
+            
+            
+            //throw new NotImplementedException();
         }
     }
 }
